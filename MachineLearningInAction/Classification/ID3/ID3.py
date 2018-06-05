0744eb8de4e8ad0ad85fcabf45ddef27447a6e69
DeluxeYang <421826500@qq.com>
2018-06-05 16:20:30 +0800

    2018-6-5

--- MachineLearningInAction/Classification/ID3/ID3.py
+++ MachineLearningInAction/Classification/ID3/ID3.py
@@ -1,4 +1,6 @@
 from math import log
+import operator
+import Tree_Plotter
 
 
 def cal_shannon_ent(data_set):  # 计算香农熵
@@ -55,5 +57,69 @@ def create_data_set():
     return data_set, labels
 
 
+def majority_count(class_list):  # 返回大多数
+    class_count = {}
+    for vote in class_list:
+        if vote not in class_count:
+            class_count[vote] = 0
+        class_count[vote] += 1
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
+
+def create_tree(data_set, labels):  # 递归创建属
+    class_list = [x[-1] for x in data_set]
+    if class_list.count(class_list[0]) == len(class_list):  #
+        return class_list[0]
+    if len(class_list[0]) == 1:
+        return majority_count(class_list)
+    best_feature = choose_best_feature_to_split(data_set)
+    best_feature_label = labels[best_feature]
+    my_tree = {best_feature_label: {}}
+    del labels[best_feature]
+    feature_values = [x[best_feature] for x in data_set]
+    unique_values = set(feature_values)
+    for value in unique_values:
+        sub_labels = labels[:]
+        my_tree[best_feature_label][value] = create_tree(split_data_set(data_set, best_feature, value), sub_labels)
+    return my_tree
+
+
+def classify(input_tree, feature_labels, test_vec):
+    first_str = list(input_tree.keys())[0]
+    second_dict = input_tree[first_str]
+    feature_index = feature_labels.index(first_str)
+    key = test_vec[feature_index]
+    value_of_feature = second_dict[key]
+    if isinstance(value_of_feature, dict):
+        class_label = classify(value_of_feature, feature_labels, test_vec)
+    else:
+        class_label = value_of_feature
+    return class_label
+
+
+def store_tree(input_tree, filename):
+    import pickle
+    fw = open(filename, 'w')
+    pickle.dump(input_tree, fw)
+    fw.close()
+
+
+def grab_tree(filename):
+    import pickle
+    fr = open(filename)
+    return pickle.load(fr)
+
+
+def lenses():
+    with open("lenses.txt") as f:
+        _lenses = [x.strip().split('\t') for x in f.readlines()]
+        lenses_labels = ['age', 'prescript', 'astigmatic', 'tearRate']
+        lenses_tree = create_tree(_lenses, lenses_labels)
+        Tree_Plotter.createPlot(lenses_tree)
+
+
 if __name__ == '__main__':
-    print(cal_shannon_ent(create_data_set()[0]))
+    # _tree = create_tree(*create_data_set())
+    # print(classify(_tree, ['no surfacing', 'flippers'], [1, 0]))
+    lenses()
