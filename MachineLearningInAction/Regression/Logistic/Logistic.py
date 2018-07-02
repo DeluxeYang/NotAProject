d8120ecc0d2bc644e76f4a5614329369eaed8874
DeluxeYang <421826500@qq.com>
2018-07-02 15:39:40 +0800

    2018-7-2

--- MachineLearningInAction/Regression/Logistic/Logistic.py
+++ MachineLearningInAction/Regression/Logistic/Logistic.py
@@ -1,4 +1,5 @@
 import numpy
+import random
 
 
 def load_data_set():
@@ -58,7 +59,75 @@ def plot_best_fit(weights):
     plt.show()
 
 
+def stochastic_grad_ascent0(data_matrix, class_labels):
+    m, n = numpy.shape(data_matrix)
+    alpha = 0.01
+    weights = numpy.ones(n)  # initialize to all ones
+    for i in range(m):
+        h = sigmoid(sum(data_matrix[i] * weights))
+        error = class_labels[i] - h
+        weights = weights + alpha * error * numpy.array(data_matrix[i])
+    return weights
+
+
+def stochastic_grad_ascent1(data_matrix, class_labels, loop=150):
+    m, n = numpy.shape(data_matrix)
+    weights = numpy.ones(n)  # initialize to all ones
+    for j in range(loop):
+        data_index = list(range(m))
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01  # alpha decreases with iteration, does not
+            rand_index = int(random.uniform(0, len(data_index)))  # go to 0 because of the constant
+            h = sigmoid(numpy.sum(data_matrix[rand_index] * weights))
+            error = class_labels[rand_index] - h
+            weights = weights + alpha * error * numpy.array(data_matrix[rand_index])
+            del (data_index[rand_index])
+    return weights
+
+
+def classify_vector(in_x, weights):
+    prob = sigmoid(sum(in_x * weights))
+    return 1.0 if prob > 0.5 else 0.0
+
+
+def colic_test():
+    fr_train = open('horseColicTraining.txt')
+    fr_test = open('horseColicTest.txt')
+    training_set = []
+    training_labels = []
+    for line in fr_train.readlines():
+        curr_line = line.strip().split('\t')
+        _list = []
+        for i in range(21):
+            _list.append(float(curr_line[i]))
+        training_set.append(_list)
+        training_labels.append(float(curr_line[21]))
+    train_weights = stochastic_grad_ascent1(numpy.array(training_set), training_labels, 1000)
+    error_count = 0
+    num_test_vec = 0.0
+    for line in fr_test.readlines():
+        num_test_vec += 1.0
+        curr_line = line.strip().split('\t')
+        _list = []
+        for i in range(21):
+            _list.append(float(curr_line[i]))
+        if int(classify_vector(numpy.array(_list), train_weights)) != int(curr_line[21]):
+            error_count += 1
+    error_rate = (float(error_count) / num_test_vec)
+    print("the error rate of this test is: %f" % error_rate)
+    return error_rate
+
+
+def multi_test():
+    num_tests = 10
+    error_sum = 0.0
+    for k in range(num_tests):
+        error_sum += colic_test()
+    print("after %d iterations the average error rate is: %f" % (num_tests, error_sum / float(num_tests)))
+
+
 if __name__ == "__main__":
-    data, label = load_data_set()
-    weight = grad_ascent(data, label)
-    plot_best_fit(weight.getA())
+    # data, label = load_data_set()
+    # weight = stochastic_grad_ascent1(data, label)
+    # plot_best_fit(weight)
+    multi_test()
